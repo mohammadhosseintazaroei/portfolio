b234a9e88fe1ff14853f5bec9789b4cca6e946bd
mohammad753 <m.h.tazaroie753@gmail.com>
2023-09-05 21:35:20 +0330

    create header and header menu

--- src/components/layouts/dashboard/header.tsx
+++ src/components/layouts/dashboard/header.tsx
@@ -1,16 +1,18 @@
-import { AppBar, Toolbar, Typography, useTheme } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, useTheme } from '@mui/material';
 
-import RouterBreadcrumbs from './breadcrumbs.component';
 import headerStyles from './header.style';
+import HeaderMenu from './header-menu';
 
 const Header = () => {
   const theme = useTheme();
   const styles = headerStyles(theme);
   return (
-    <AppBar position="sticky" color="secondary" enableColorOnDark sx={styles.headerWrapper}>
+    <AppBar position="sticky" enableColorOnDark sx={styles.headerWrapper}>
       <Toolbar sx={styles.headerToolbar}>
-        <Typography variant="h6" noWrap component="div" sx={styles.breadcrumbsLink}>
-          <RouterBreadcrumbs />
+        <Typography variant="h6" noWrap component="div">
+          <Box>
+            <HeaderMenu />
+          </Box>
         </Typography>
       </Toolbar>
     </AppBar>
