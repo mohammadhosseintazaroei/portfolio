ded7bab00ca8499cae2d956e409ef02d80072191
mohammad753 <m.h.tazaroie753@gmail.com>
2023-09-14 00:44:48 +0330

    change home icon and remove usless files

--- src/components/layouts/dashboard/header.style.ts
+++ src/components/layouts/dashboard/header.style.ts
@@ -1,25 +1,37 @@
-import { type Theme } from '@mui/material';
-const styles = (theme: Theme) => {
-  return {
-    headerWrapper: {
-      boxShadow: 'none',
-      background: 'none',
-    },
-    headerToolbar: {
-      display: 'flex',
-      minHeight: '72px !important',
-      justifyContent: 'space-between',
-      color: theme.palette.neutral.light,
-      py: '42px',
-      paddingLeft: '60px !important',
-    },
+import { type SxProps, type Theme } from '@mui/material';
 
-    searchIconWrapper: {
-      right: '16px',
-      padding: '0',
-      color: theme.palette.neutral.dark,
+const headerStyles: Record<string, SxProps<Theme>> = {
+  logo: {
+    transition: '.2s',
+    width: 'auto',
+    borderRadius: '0',
+    height: '100px',
+    cursor: 'pointer',
+    '& img': {
+      width: 'auto',
     },
-  };
+  },
+  headerWrapper: {
+    boxShadow: 'none',
+    background: 'none',
+  },
+  headerToolbar: {
+    display: 'flex',
+    minHeight: '72px !important',
+    justifyContent: 'start',
+    color: (theme) => theme.palette.neutral.light,
+    py: 3,
+    paddingLeft: '50px !important',
+  },
+
+  searchIconWrapper: {
+    right: '16px',
+    padding: '0',
+    color: (theme) => theme.palette.neutral.dark,
+  },
+  menuContainer: {
+    marginLeft: '50px',
+  },
 };
 
-export default styles;
+export default headerStyles;
--- src/components/layouts/dashboard/header.tsx
+++ src/components/layouts/dashboard/header.tsx
@@ -1,17 +1,122 @@
-import { AppBar, Box, Toolbar, Typography, useTheme } from '@mui/material';
+import { AppBar, Avatar, Box, Link, Toolbar, Typography } from '@mui/material';
 
+import { useEffect, useRef, useState } from 'react';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+import Logo from '../../../assets/images/logo.png';
+import { panelEntities } from '../../../router/entities';
+import { sxSeparator } from '../../../utils/sxSeparator';
 import headerStyles from './header.style';
-import HeaderMenu from './header-menu';
 
 const Header = () => {
-  const theme = useTheme();
-  const styles = headerStyles(theme);
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  const [menuItemLeftOffset, setMenuItemLeftOffset] = useState<number>();
+  const [menuItemWidth, setMenuItemWidth] = useState<number>();
+  const [homeIconTop, setHomeIconTop] = useState<number>();
+  const [pageIndex, setPageIndex] = useState<number | null>(null);
+  const menu = useRef<HTMLDivElement>();
+  const homeIconRef = useRef<HTMLDivElement>();
+
+  const handleCloseNavMenu = (e: React.MouseEvent) => {
+    const target = e.target as HTMLDivElement;
+    setMenuItemLeftOffset(target.offsetLeft);
+    setMenuItemWidth(target.clientWidth);
+    if (currentUrl === '/') {
+      setMenuItemWidth(homeIconRef.current?.clientWidth);
+      setMenuItemLeftOffset(target?.offsetLeft);
+      setHomeIconTop(target?.clientHeight + target?.offsetTop);
+    }
+  };
+
+  useEffect(() => {
+    {
+      panelEntities.map((page, index) => {
+        if (currentUrl === page.path) setPageIndex(index);
+        if (currentUrl === '/') {
+          setPageIndex(null);
+          setMenuItemWidth(homeIconRef.current?.clientWidth);
+          setMenuItemLeftOffset(homeIconRef.current?.offsetLeft);
+          setHomeIconTop((homeIconRef.current?.clientHeight ?? 0) + (homeIconRef.current?.offsetTop ?? 0));
+        }
+      });
+    }
+
+    if (pageIndex != null) {
+      setMenuItemLeftOffset((menu.current?.children[pageIndex] as HTMLElement)?.offsetLeft);
+      setMenuItemWidth(menu.current?.children[pageIndex]?.clientWidth);
+    }
+  });
+
   return (
-    <AppBar position="sticky" enableColorOnDark sx={styles.headerWrapper}>
-      <Toolbar sx={styles.headerToolbar}>
-        <Typography variant="h6" noWrap component="div">
+    <AppBar position="sticky" enableColorOnDark sx={headerStyles.headerWrapper}>
+      <Toolbar sx={headerStyles.headerToolbar}>
+        <Link component={RouterLink} to="/">
+          <Box ref={homeIconRef} onClick={(e) => handleCloseNavMenu(e)}>
+            <Avatar src={Logo} sx={headerStyles.logo} />
+          </Box>
+        </Link>{' '}
+        <Typography sx={headerStyles.menuContainer} variant="h6" noWrap component="div">
           <Box>
-            <HeaderMenu />
+            <Box>
+              <Box sx={{ flexGrow: 1, display: 'flex' }}>
+                <Box
+                  className={`border ${currentUrl == '/' && 'home'}`}
+                  sx={{
+                    background: '#fff',
+                    width: `${menuItemWidth ?? 0}px`,
+                    height: '6px',
+                    borderRadius: '10px 10px 100px 100px',
+                    top: `${menuItemWidth ? '0' : '-10'}px`,
+                    position: 'absolute',
+                    left: `${menuItemLeftOffset ?? 0}px`,
+                    color: '#fff0',
+                    transition: '500ms',
+
+                    '&.border:has(~ .pageClicked)': {
+                      color: 'red',
+                    },
+                    '&.home': {
+                      top: `${homeIconTop ? homeIconTop + 15 : 0}px`,
+                      height: '8px',
+                    },
+                  }}
+                ></Box>
+
+                <Box display={'flex'} ref={menu}>
+                  {panelEntities.map((page, index) => {
+                    return (
+                      <Link component={RouterLink} to={page.path} sx={{ marginRight: '64px' }}>
+                        <Typography
+                          variant="heading2"
+                          key={index}
+                          onClick={(e) => handleCloseNavMenu(e)}
+                          className={`${currentUrl === page.path && 'pageClicked'}`}
+                          sx={[
+                            {
+                              display: 'block',
+                              fontWeight: '400',
+
+                              cursor: 'pointer',
+                              '&:hover': {
+                                color: (theme) => theme.palette.neutral.light,
+                              },
+                              '&.pageClicked ~ .border': {
+                                display: 'flex',
+                              },
+                            },
+                            sxSeparator(
+                              currentUrl === page.path && { color: (theme) => theme.palette.neutral.lighter }
+                            ),
+                          ]}
+                        >
+                          {page.title}
+                        </Typography>
+                      </Link>
+                    );
+                  })}
+                </Box>
+              </Box>
+            </Box>
           </Box>
         </Typography>
       </Toolbar>
