74cfcfc68e3c44700d8a8de96d6e3ea6a8c61cac
mohammad753 <m.h.tazaroie753@gmail.com>
2023-10-30 16:54:26 +0330

    create github page header

--- src/components/github/github-page.tsx
+++ src/components/github/github-page.tsx
@@ -1,16 +1,30 @@
 import { Box, CircularProgress, Grid } from '@mui/material';
-import styles from './github.style';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
+import react from '../../assets/images/Vector.png';
+import css from '../../assets/images/css 4.png';
+import express from '../../assets/images/express-js 1.png';
+import github from '../../assets/images/github 2.png';
+import gql from '../../assets/images/graphql 1.png';
+import html from '../../assets/images/html 1.png';
+import nodejs from '../../assets/images/nodejs 1.png';
+import typescript from '../../assets/images/typescript 1.png';
 import { TypoText } from '../globals/typo-text';
-
+import styles from './github.style';
 interface Repo {
   name: string;
   description: string;
   topics: string[];
 }
+interface ProfileData {
+  name: string;
+  avatar_url: string;
+  login: string;
+  bio: string;
+}
 export function GitHubPage() {
   const [repos, setRepos] = useState<Repo[]>();
+  const [profileData, setProfileData] = useState<ProfileData>();
   useEffect(() => {
     axios
       .get('https://api.github.com/users/mohammadhosseintazaroei/repos')
@@ -21,12 +35,54 @@ export function GitHubPage() {
       .catch((error) => {
         console.error('Error fetching repositories:', error);
       });
+
+    axios
+      .get('https://api.github.com/users/mohammadhosseintazaroei')
+      .then((response) => {
+        setProfileData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching repositories:', error);
+      });
   }, []);
 
   return (
     <Box sx={styles.gitHubStyles}>
+      <Grid container sx={styles.profileContainer}>
+        <Grid item xs={6}>
+          {profileData?.avatar_url && <Box component={'img'} src={profileData?.avatar_url} sx={styles.profileImage} />}
+          <Box>
+            <Box sx={styles.profileName}>{profileData?.name}</Box>
+          </Box>
+          <Box>
+            <Box sx={styles.loginName}>{profileData?.login}</Box>{' '}
+          </Box>
+          <Box>
+            <Box sx={styles.profileBio}>{profileData?.bio}</Box>{' '}
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box sx={styles.topImages}>
+            <Box component={'img'} src={express} sx={styles.express} />
+            <Box component={'img'} src={nodejs} sx={styles.nodejs} />
+            <Box component={'img'} src={html} sx={styles.html} />
+          </Box>
+
+          <Box sx={styles.githubWrapper}>
+            <Box component={'img'} src={github} sx={styles.github} />
+          </Box>
+          <Box component={'img'} src={github} sx={styles.githubFake} />
+
+          <Box sx={styles.bottomImages}>
+            <Box component={'img'} src={css} sx={styles.css} />
+            <Box component={'img'} src={react} sx={styles.react} />
+            <Box component={'img'} src={typescript} sx={styles.ts} />
+            <Box component={'img'} src={gql} sx={styles.gql} />
+          </Box>
+        </Grid>
+      </Grid>
       {repos?.length ? (
-        <Grid container columnGap={3}>
+        <Grid container columnGap={3} sx={styles.reposGridContainer}>
           {repos?.map((repo) => {
             return (
               <>
--- src/components/github/github.style.tsx
+++ src/components/github/github.style.tsx
@@ -4,6 +4,9 @@ const gitHubStyles: Record<string, SxProps<Theme>> = {
   gitHubStyles: {
     p: 5,
   },
+  reposGridContainer: {
+    justifyContent: 'center',
+  },
   reposContainer: {
     mb: 5,
     background: '#161616',
@@ -36,6 +39,86 @@ const gitHubStyles: Record<string, SxProps<Theme>> = {
   },
   repoDescription: {
     color: (theme) => theme.palette.neutral.main,
+  },
+  profileImage: {
+    borderRadius: '50%',
+    width: '386',
+    mb: 2,
+  },
+  profileName: {
+    color: '#fff',
+    fontSize: 30,
+    mb: 1,
+  },
+  loginName: {
+    fontSize: 27,
+    color: '#7F7F7F',
+    fontWeight: '500',
+    mb: 2,
+  },
+  profileBio: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'lighter',
+  },
+  profileContainer: {
+    mb: 30,
+    textAlign: 'center',
+  },
+  topImages: {
+    position: 'relative',
+    mb: 14,
+  },
+  express: {
+    position: 'absolute',
+    top: 40,
+    left: 30,
+  },
+  nodejs: {
+    position: 'absolute',
+    top: 0,
+    left: 280,
+  },
+  html: {
+    position: 'absolute',
+    top: 250,
+    left: 450,
+  },
+  githubWrapper: {
+    position: 'relative',
+  },
+  github: {
+    p: 10,
+    position: 'absolute',
+    left: 0,
+  },
+  githubFake: {
+    p: 10,
+    visibility: 'hidden',
+  },
+  bottomImages: {
+    position: 'relative',
+  },
+  css: {
+    position: 'absolute',
+    top: -350,
+    left: -100,
+  },
+  react: {
+    position: 'absolute',
+    top: -150,
+    left: -60,
+  },
+
+  ts: {
+    position: 'absolute',
+    top: -70,
+    left: 150,
+  },
+  gql: {
+    position: 'absolute',
+    top: -100,
+    left: 350,
   },
 };
 
